0b60cdcfb2bf7d389575e5e13a39bbdd7cd6011a
KyumKyum <mycheesepasta@gmail.com>
2024-12-08 18:16:34 +0900

    feat: main page

--- shieldbid-fe/src/App.tsx
+++ shieldbid-fe/src/App.tsx
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
+=======
+>>>>>>> 5451f62 (feat: main page)
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import VerifyGroth16Page from "./pages/VerifyGroth16Page";
 import SignupPage from "./pages/MainPage";
@@ -7,14 +10,21 @@ import CreateAuctionPage from "./pages/CreateAuctionPage";
 import AuctionListPage from "./pages/AuctionListPage";
 import AuctionDetailPage from "./pages/AuctionDetailPage";
 import ProveGroth16Page from "./pages/ProveGroth16Page";
+import MainPage from "./pages/MainPage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
+      <div className="flex flex-col">
+        <nav className="flex">
+          <ul className="flex w-full justify-center items-center h-24 text-white gap-24 font-dream">
+            <li>
+              <Link to="/">Main Page</Link>
+            </li>
+            <li>
+              <Link to="/signUp">Sign Up</Link>
+            </li>
             <li>
               {isLoggedIn ? (
                 <Link to="/">Home</Link>
